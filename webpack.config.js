f6c0e730aa7c81d98237b9281bf0dca6d7ac34c6
Josh S <aaabccc87@protonmail.com>
2022-09-16 12:36:41 -0400

    Changed entry to drop-down-menu.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './drop-down-menu.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, './dist'),
